dc0112cd5dd0d0545ff9b890a32d5184f6b22d6b
unlartigau <unlartigau@c05c6ade-ccf1-41d0-bc36-3893a9020871>
2021-04-28 13:23:06 +0000

    Ajout de l'énumération TypePlanete.

---
+++ source/StellarC#/Stellar/StellarModele/TypePlanete.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StellarModele
+{
+    public enum TypePlanete
+    {
+        Tellurique,
+        Gazeuse,
+        Naine
+    }
+}
