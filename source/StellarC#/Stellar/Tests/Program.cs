c135c104119d3b88750867674a8caa55c8e5461c
jetremblay <jeremy-tremblay@outlook.fr>
2021-04-29 21:16:41 +0000

    Ajout de la classe Astre et la classe de test unitaire Test.

---
+++ source/StellarC#/Stellar/Tests/Program.cs
@@ -0,0 +1,14 @@
+﻿using StellarModele;
+using System;
+
+namespace Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Astre astre = new Astre("UnAstre", 200, 2, 3600, false, true);
+            Console.WriteLine(astre);
+        }
+    }
+}
