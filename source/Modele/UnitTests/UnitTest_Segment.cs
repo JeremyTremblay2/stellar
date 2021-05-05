426595b9df5567b0d59cb1ff05f190a584498be6
unlartigau <unlartigau@c05c6ade-ccf1-41d0-bc36-3893a9020871>
2021-05-05 13:22:43 +0000

    Ajout de la classe Constellation et de la méthode equals dans Point et Segment.

--- source/Modele/UnitTests/UnitTest_Segment.cs
+++ source/Modele/UnitTests/UnitTest_Segment.cs
@@ -17,5 +17,22 @@ namespace UnitTests
             Segment unSegment = new Segment(pt1, pt2);
             Assert.Equal(0, unSegment.Point1.X);
         }
+
+        /// <summary>
+        /// test de la method Equals
+        /// </summary>
+        [Fact]
+        public void testEqual()
+        {
+            Point pt1 = new Point(0, 0);
+            Point pt2 = new Point(1, 1);
+            Point pt3 = new Point(0, 0);
+            Point pt4 = new Point(1, 1);
+            Segment seg1 = new Segment(pt1, pt2);
+            Segment seg2 = new Segment(pt3, pt4);
+            Segment seg3 = new Segment(pt1, pt3);
+            Assert.True(seg1.Equals(seg2));
+            Assert.False(seg1.Equals(seg3));
+        }
     }
 }
