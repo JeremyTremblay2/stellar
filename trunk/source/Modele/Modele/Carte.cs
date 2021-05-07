29b891aff47fb81d312f617c0ebd19c5738bbc59
jetremblay <jetremblay@c05c6ade-ccf1-41d0-bc36-3893a9020871>
2021-05-07 06:21:17 +0000

    Modification du diagramme de classes, modification de la classe Carte.

---
+++ trunk/source/Modele/Modele/Carte.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Modele
+{
+    public class Carte
+    {
+        private Dictionary<Point, Astre> lesAstres;
+        private List<Constellation> lesConstellations;
+
+        public Carte(bool avecCreation)
+        {
+            lesAstres = new Dictionary<Point, Astre>();
+            lesConstellations = new List<Constellation>();
+
+            if (avecCreation)
+            {
+                //Ajouter des astres et constellations.
+            }
+        }
+        
+        public void ajouterUneEtoile(Point position)
+        {
+            lesAstres[position] = null;
+        }
+
+        public void supprimerUneConstellation(Point position)
+        {
+            
+        }
+    }
+}
