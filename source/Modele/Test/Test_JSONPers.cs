243b9b0ff778442c386a6d7e265a95d193ef7bb4
jetremblay <jeremy-tremblay@outlook.fr>
2021-06-01 06:28:48 +0000

    Ajout d'un fichier de test oublié hier.

---
+++ source/Modele/Test/Test_JSONPers.cs
@@ -0,0 +1,31 @@
+﻿using Modele;
+using JSONPersistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Tests_Fonctionnels
+{
+    public static class Test_JSONPers
+    {
+        public static void TestChargement()
+        {
+            //Création du manager et chargement du stub.
+            Manager manager = new Manager(new Stub.Stub(), new Stub.Stub());
+            manager.ChargeDonnees();
+            Console.WriteLine(manager);
+
+            //Sauvegarde en JSON des données.
+            manager.Persistance = new JSONPers();
+            manager.SauvegardeDonnees();
+
+            //Chargement des données précédemment sauvegardées après instanciation d'un nouveau manager.
+            Manager manager2 = new Manager(new JSONPers(), new DataContractPersistance.DataContractPers());
+            manager2.ChargeDonnees();
+            Console.WriteLine(manager2);
+        }
+    }
+}
