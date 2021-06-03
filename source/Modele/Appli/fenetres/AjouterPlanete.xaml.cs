f4048a07115730cc6b9b651fd25a3012aa8a5368
jetremblay <jeremy-tremblay@outlook.fr>
2021-06-03 20:39:40 +0000

    Ajouts mineurs accessibilité comme des textes qui apparaissent lors du survol de boutons.

--- source/Modele/Appli/fenetres/AjouterPlanete.xaml.cs
+++ source/Modele/Appli/fenetres/AjouterPlanete.xaml.cs
@@ -37,8 +37,7 @@ namespace Appli.fenetres
         public AjouterPlanete()
         {
             InitializeComponent();
-            Vie.IsEnabled = false;
-            var planete = new Planete("", "", 0, 0, 0, "dfgd", false, "", TypePlanete.Tellurique, true, "planète.jpg");
+            var planete = new Planete("", "", 0, 0, 0, "", false, "", TypePlanete.Tellurique, true, "planète.jpg");
             LaPlanete = new Planete(planete.Nom, planete.Description, planete.Age, planete.Masse, planete.Temperature, planete.Vie,
                 planete.EauPresente, planete.Systeme, planete.Type, planete.Personnalise, planete.Image);
             LaPlaneteEditable = new Planete(LaPlanete.Nom, LaPlanete.Description, LaPlanete.Age, LaPlanete.Masse, LaPlanete.Temperature, LaPlanete.Vie,
